e72db9b78d25c4af8fb4c7ec8623696014a506a4
Sydney Ball <sydneyball@sydneys-mbp.american.edu>
2025-02-07 10:58:10 -0500

    Print the mean and SD

--- Ball_HW.R
+++ Ball_HW.R
@@ -1,3 +1,4 @@
 random_numbers <- rnorm(100, mean = 25, sd = 1)
 mean_value <- mean(random_numbers)
 sd_value <- sd(random_numbers)
+print(paste(The Mean is, mean_value, and the Standard Deviation is, sd_value))
